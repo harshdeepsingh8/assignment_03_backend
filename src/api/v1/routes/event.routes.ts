b2a62b6466d4f4fab0435ec5d7b35137238a5fbb
Harshdeep <haeshdeepsingh8@rrc.ca>
2026-02-24 19:10:02 -0600

    feat: register CRUD routes for event API.

--- src/api/v1/routes/event.routes.ts
+++ src/api/v1/routes/event.routes.ts
@@ -1,12 +1,23 @@
 import express from "express";
+
 import { createEventSchema } from "../validation/event.validation";
 import { validate } from "../middleware/validate.middleware";
-import { createEventController } from "../controllers/event.controller";
 
-const router = express.Router();
+import {
+  createEventController,
+  getAllEventsController,
+  getEventByIdController,
+  updateEventController,
+  deleteEventController,
+} from "../controllers/event.controller";
 
-router.post(
-  "/events",
-  validate(createEventSchema),
-  createEventController
-);
+const router = express.Router(); // ✅ MUST BE BEFORE USING
+
+router.post("/events", validate(createEventSchema), createEventController);
+
+router.get("/events", getAllEventsController);
+router.get("/events/:id", getEventByIdController);
+router.put("/events/:id", updateEventController);
+router.delete("/events/:id", deleteEventController);
+
+export default router;
